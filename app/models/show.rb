dafb4666fd256dadc33f6623d337695836b63da0
Katie Moosman <moosman.katie@gmail.com>
2019-02-20 05:21:32 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,8 +5,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    rating = Show.highest_rating
-    Show.where("rating > ?", rating)
+    Show.where("rating > ?", 10)
   end
 
 end
